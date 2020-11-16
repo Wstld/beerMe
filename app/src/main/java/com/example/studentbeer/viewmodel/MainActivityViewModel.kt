7b12751b174a7c999445ce9afc2531261e08d054
Wstld <none@none.se>
2020-11-16 14:16:18 +0100

    "getUserReview" function added, which currently: Displays custom alertdialog Fills view with barmodel data.

--- app/src/main/java/com/example/studentbeer/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/studentbeer/viewmodel/MainActivityViewModel.kt
@@ -2,7 +2,10 @@ package com.example.studentbeer.viewmodel
 
 import android.app.Application
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.util.Log
+import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.core.location.LocationManagerCompat
 import androidx.lifecycle.LiveData
@@ -12,9 +15,11 @@ import androidx.lifecycle.viewModelScope
 import com.example.studentbeer.data.DataRepository
 import com.example.studentbeer.data.models.BarModel
 import com.example.studentbeer.data.models.LocationModel
+import com.example.studentbeer.databinding.UserReviewBinding
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.*
 import kotlinx.coroutines.android.awaitFrame
 
@@ -27,10 +32,17 @@ class MainActivityViewModel(private val dataRepository: DataRepository) : ViewMo
         map.clear()
     }
 
-    fun getUserReview(context: Context){
-
-
-
+    fun getUserReview(context: Context,bar:BarModel){
+        val reviewBinding = UserReviewBinding.inflate(LayoutInflater.from(context))
+        reviewBinding.apply {
+            userPriceInputCardBarTitle.text = bar.barName
+            userReviewCardBarTitle.text = bar.barName
+            userPriceInputCardBeerPrice.text = bar.beerPrice.toString()
+        }
+        MaterialAlertDialogBuilder(context)
+            .setView(reviewBinding.root)
+            .setBackground(ColorDrawable(Color.TRANSPARENT))
+            .show()
     }
 
 
