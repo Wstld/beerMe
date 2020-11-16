ea943262a1d464960c6787a72955c664c051c77d
Wstld <none@none.se>
2020-11-16 15:33:27 +0100

    Dialog functionality added, user inputs recognised and sent to dummy firebase connection function.

--- app/src/main/java/com/example/studentbeer/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/studentbeer/viewmodel/MainActivityViewModel.kt
@@ -1,27 +1,23 @@
 package com.example.studentbeer.viewmodel
 
-import android.app.Application
+
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.Toast
-import androidx.core.location.LocationManagerCompat
-import androidx.lifecycle.LiveData
+
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
-import androidx.lifecycle.viewModelScope
+
 import com.example.studentbeer.data.DataRepository
 import com.example.studentbeer.data.models.BarModel
-import com.example.studentbeer.data.models.LocationModel
+
+import com.example.studentbeer.data.models.UserReviewModel
 import com.example.studentbeer.databinding.UserReviewBinding
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import kotlinx.coroutines.*
-import kotlinx.coroutines.android.awaitFrame
+
 
 class MainActivityViewModel(private val dataRepository: DataRepository) : ViewModel() {
     val currentPos = dataRepository.liveLocationData
@@ -32,17 +28,45 @@ class MainActivityViewModel(private val dataRepository: DataRepository) : ViewMo
         map.clear()
     }
 
+    fun sendUserReview(userReview: UserReviewModel){
+        //Connection with firebase
+        Log.d("userREVIEW", "sendUserReview: ${userReview.barName} rating:${userReview.userRating} barid:${userReview.barFireBaseId} price:${userReview.userDefinedBeerPrice} ")
+    }
+
     fun getUserReview(context: Context,bar:BarModel){
         val reviewBinding = UserReviewBinding.inflate(LayoutInflater.from(context))
+
+        val dialog = MaterialAlertDialogBuilder(context)
+            .setView(reviewBinding.root)
+            .setBackground(ColorDrawable(Color.TRANSPARENT))
+            .create()
+
         reviewBinding.apply {
             userPriceInputCardBarTitle.text = bar.barName
             userReviewCardBarTitle.text = bar.barName
             userPriceInputCardBeerPrice.text = bar.beerPrice.toString()
+            userPriceInputCardPlusBtn.setOnClickListener {
+               val newPrice =  userPriceInputCardBeerPrice.text.toString().toInt() + 1
+                userPriceInputCardBeerPrice.text = newPrice.toString()
+            }
+            userPriceInputCardMinusBtn.setOnClickListener {
+                if (userPriceInputCardBeerPrice.text.toString().toInt()>0){
+                    val newPrice = userPriceInputCardBeerPrice.text.toString().toInt() -1
+                    userPriceInputCardBeerPrice.text = newPrice.toString()
+                }
+            }
+            sendReviewBtn.setOnClickListener {
+                sendUserReview(
+                    UserReviewModel(
+                        barFireBaseId = bar.id,
+                        barName = bar.barName,
+                        userRating = userReviewCardUserRating.rating.toDouble(),
+                        userDefinedBeerPrice = userPriceInputCardBeerPrice.text.toString().toInt()
+                    ))
+                dialog.dismiss()
+            }
         }
-        MaterialAlertDialogBuilder(context)
-            .setView(reviewBinding.root)
-            .setBackground(ColorDrawable(Color.TRANSPARENT))
-            .show()
+            dialog.show()
     }
 
 
