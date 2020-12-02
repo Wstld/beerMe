7ec9e0d15f20cff600c38b2cd74048468c80c3ae
WaadSulaiman <waad.sulaiman@outlook.com>
2020-12-02 12:20:29 +0100

    Now the app only shows bars within 2 km.

--- app/src/main/java/com/example/studentbeer/other/tools/FilterAalgorithm.kt
+++ app/src/main/java/com/example/studentbeer/other/tools/FilterAalgorithm.kt
@@ -1,9 +1,29 @@
 package com.example.studentbeer.other.tools
 
+import android.location.Location
+import android.util.Log
 import com.example.studentbeer.data.models.BarAndDistanceModel
 
 
 object FilterAlgorithm {
+
+    fun checkDistance(
+        currentLat: Double,
+        currentLong: Double,
+        destinationLat: Double,
+        destinationLong: Double
+    ): Boolean {
+        val locationA = Location("Point A")
+        val locationB = Location("Point B")
+        locationA.latitude = currentLat
+        locationA.longitude = currentLong
+        locationB.latitude = destinationLat
+        locationB.longitude = destinationLong
+        val distance = locationA.distanceTo(locationB)
+        Log.d("!!!", distance.toString())
+        if (distance > 2000F) return false
+        return true
+    }
 
     fun sortByPrice(list: MutableList<BarAndDistanceModel>): MutableList<BarAndDistanceModel> {
         list.sortBy {
